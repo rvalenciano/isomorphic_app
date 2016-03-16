3175b212f54f5a8efaf491d0aab4cfc18fa23882
Randall Valenciano Fallas <ravf.226@gmail.com>
2016-03-16 13:17:00 -0600

    corrects .gitignore

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = new express();
 app.get('/', function(req, res) {
-  res.render('./../app/index.html', {});
+  res.render('./../app/index.ejs', {});
 })
+. use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
